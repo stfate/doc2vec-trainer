127fa63d4450feacebb06054a7ab1d86ccc65b45
stfate <stfate@gmail.com>
2019-12-20 07:54:58 +0900

    add train_wikipedia

---
+++ train_wikipedia.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+WIKIPEDIA_DUMP_PATH=../../dataset/Wikipedia/jawiki-latest-pages-articles.xml.bz2
+LANG=ja
+DIC_PATH=/usr/local/lib/mecab/dic/mecab-ipadic-neologd
+OUTPUT_PATH=model/jawiki-d2v-model/doc2vec.gensim.model
+SIZE=400
+WINDOW=8
+MIN_COUNT=5
+DM=0
+EPOCH=5
+
+# train doc2vec model
+python train_wikipedia.py -o $OUTPUT_PATH --dictionary-path=$DIC_PATH --wikipedia-dump-path=$WIKIPEDIA_DUMP_PATH --lang=$LANG --size=$SIZE --window=$WINDOW --min-count=$MIN_COUNT --dm=$DM --epoch=$EPOCH
